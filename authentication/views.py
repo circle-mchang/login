ff178ba2bef61080fc0410d029048700b9e32f42
Min Chang <min.chang@Min-Chang-MacBook-Pro.local>
2022-07-13 16:40:43 -0700

    Add initial files and htmls

---
+++ authentication/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . import views
+
+# Create your views here.
+def home(request):
+    return render(request, "authentication/index.html")
+
+def signup(request):
+    return render(request, "authentication/signup.html")
+
+def signin(request):
+    return render(request, "authentication/signin.html")
+
+def signout(request):
+    return render(request, "authentication/signout.html")
